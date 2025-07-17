54d021a232a740abe9306ac90d3ff6b757fc4abc
Streamtg <202310475@issu.edu.do>
2025-07-17 14:01:45 -0400

    Update routes.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
 import (
+	"EverythingSuckz/fsb/internal/types"
+	"EverythingSuckz/fsb/internal/utils"
 	"fmt"
+	"html/template"
 	"net/http"
 	"path/filepath"
 	"reflect"
-	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -24,59 +27,68 @@ type allRoutes struct {
 	log *zap.Logger
 }
 
+// Load is responsible for dynamically loading all methods in allRoutes struct
 func Load(log *zap.Logger, r *gin.Engine) {
 	log = log.Named("routes")
 	defer log.Sugar().Info("Loaded all API Routes")
-
 	route := &Route{Name: "/", Engine: r}
 	route.Init(r)
-
 	Type := reflect.TypeOf(&allRoutes{log})
 	Value := reflect.ValueOf(&allRoutes{log})
-
 	for i := 0; i < Type.NumMethod(); i++ {
 		Type.Method(i).Func.Call([]reflect.Value{Value, reflect.ValueOf(route)})
 	}
 }
 
-// Ruta raíz
-func (r *allRoutes) Root(route *Route) {
-	route.Engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "FileStreamBot is running",
-			"ok":      true,
+// Root route - basic health check
+func (a *allRoutes) Root(r *Route) {
+	a.log.Debug("Loading root route")
+	r.Engine.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, types.RootResponse{
+			Message: "Server is running.",
+			Ok:      true,
+			Uptime:  utils.TimeFormat(uint64(time.Since(utils.StartTime).Seconds())),
+			Version: utils.VersionString,
 		})
 	})
-	r.log.Info("Loaded root route")
 }
 
-// Ruta que sirve archivos desde ./downloads/
-func (r *allRoutes) Stream(route *Route) {
-	route.Engine.GET("/file/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		// Directorio base de archivos
-		filePath := filepath.Join("downloads", id)
-
-		c.FileAttachment(filePath, id)
+// StreamRoute serves the media file stream (existing route in original bot)
+func (a *allRoutes) Stream(r *Route) {
+	a.log.Debug("Loading stream route")
+	r.Engine.GET("/stream/:messageID", func(c *gin.Context) {
+		messageID := c.Param("messageID")
+		filePath := filepath.Join("downloads", messageID)
+		c.File(filePath)
 	})
-	r.log.Info("Loaded file stream route")
+	a.log.Info("Loaded stream route")
 }
 
-// Ruta que muestra el template del reproductor
-func (r *allRoutes) StreamPlayer(route *Route) {
-	route.Engine.GET("/stream/:id", func(c *gin.Context) {
-		fileID := c.Param("id")
-		isVideo := strings.HasSuffix(fileID, ".mp4") || strings.HasSuffix(fileID, ".webm") || strings.HasSuffix(fileID, ".mov")
-		isAudio := strings.HasSuffix(fileID, ".mp3") || strings.HasSuffix(fileID, ".ogg") || strings.HasSuffix(fileID, ".wav")
+// StreamPlayer renders HTML player for the given messageID
+func (a *allRoutes) StreamPlayer(r *Route) {
+	a.log.Debug("Loading player template route")
 
-		fileURL := fmt.Sprintf("/file/%s", fileID)
+	// Ruta nueva, sin conflicto
+	r.Engine.GET("/player/:messageID", func(c *gin.Context) {
+		messageID := c.Param("messageID")
 
-		c.HTML(http.StatusOK, "player.html", gin.H{
-			"FileID":  fileID,
-			"FileURL": fileURL,
-			"IsVideo": isVideo,
-			"IsAudio": isAudio,
-		})
+		tmpl, err := template.ParseFiles("templates/player.html")
+		if err != nil {
+			a.log.Error("Template parsing failed", zap.Error(err))
+			c.String(http.StatusInternalServerError, "Template error")
+			return
+		}
+
+		// Datos que se pasan al HTML
+		data := gin.H{
+			"StreamURL": fmt.Sprintf("/stream/%s", messageID),
+			"MessageID": messageID,
+		}
+
+		err = tmpl.Execute(c.Writer, data)
+		if err != nil {
+			a.log.Error("Template execution failed", zap.Error(err))
+		}
 	})
-	r.log.Info("Loaded stream player route")
+	a.log.Info("Loaded player route")
 }
